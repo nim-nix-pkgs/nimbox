4db46d394a1fb8fa41670d55264e831d57e4ad1c
hugosenari <hugosenari@gmail.com>
2022-03-12 04:25:40 -0300

    chore: re index nimbox

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''A Rustbox-inspired termbox wrapper'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."nimbox-master".url = "path:./master";
+  inputs."nimbox-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimbox-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
