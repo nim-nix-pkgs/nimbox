94609e741b83de0158f91b9e776c0382d8c0b561
hugosenari <hugosenari@gmail.com>
2022-05-12 11:54:40 -0300

    chore: re index nimbox

--- flake.nix
+++ flake.nix
@@ -7,6 +7,14 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."nimbox-master".dir   = "master";
+  inputs."nimbox-master".owner = "nim-nix-pkgs";
+  inputs."nimbox-master".ref   = "master";
+  inputs."nimbox-master".repo  = "nimbox";
+  inputs."nimbox-master".type  = "github";
+  inputs."nimbox-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimbox-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
